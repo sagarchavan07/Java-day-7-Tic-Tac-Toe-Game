28bddc9631e44b242ca07e0f96405b10d883e37b
sagarchavan07 <chavansagar96044@gmail.com>
2022-07-12 11:10:36 +0530

    created board of char size 10

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -4,5 +4,8 @@ public class TicTacToe {
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
 
+        char []board=new char[]{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',};
+
+
     }
 }
