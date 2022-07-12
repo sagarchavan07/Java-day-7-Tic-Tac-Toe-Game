c2fd99757ff22de5fc52fb46c8fc4ce713b211e2
sagarchavan07 <chavansagar96044@gmail.com>
2022-07-12 12:42:58 +0530

    Added : check free space before move

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -7,9 +7,8 @@ public class TicTacToe {
     static char player2letter=' ';
     static Scanner scanner=new Scanner(System.in);
     static char []board=new char[]{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',};
+
     public static void main(String[] args) {
-
-
         System.out.println("Welcome to Tic Tac Toe Game");
         
         player1letter=chooseLetter();
