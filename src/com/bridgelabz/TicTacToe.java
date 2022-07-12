dcf987a045d4c78ada1093db5f09a717a79d4103
sagarchavan07 <chavansagar96044@gmail.com>
2022-07-12 11:28:24 +0530

    player choose letter O or X

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,11 +1,28 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+
     public static void main(String[] args) {
+        char player1letter=' ';
+        char player2letter=' ';
+
         System.out.println("Welcome to Tic Tac Toe Game");
 
         char []board=new char[]{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',};
 
+        player1letter=chooseLetter();
+        player2letter=(player1letter == 'O') ? 'X': 'O';
 
+        System.out.println("player1letter = "+player1letter);
+        System.out.println("player2letter = "+player2letter);
+
+    }
+
+    static char chooseLetter(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("choose letter : O or X");
+        return scanner.next().charAt(0);
     }
 }
